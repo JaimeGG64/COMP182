003ba6430e914e81cbbe8af644866ded5293269c
Jaime Garcia Garcia <jgarcia16432@gmail.com>
2019-09-05 10:12:00 -0700

    sort 2 now generate 'double' type number instead of 'integers'

--- 09-03-2019/sort2/sort2.java
+++ 09-03-2019/sort2/sort2.java
@@ -20,34 +20,39 @@ public class sort2 {
 	}
 
 	// Generate and print n integers in the range of a....b
-	private void genarr(int arr[], int n, int a, int b, int k) {
-		int range = b - a + 1;
+	private void genarr(double arr[], int n, double a, double b, int k) {
+		double range = b - a + 1;
 		for (int i = 0; i < n; i++)
-			arr[i] = ((int) (Math.random() * range)) + a;
+			arr[i] = ((double) (Math.random() * range)) + a;
 		prt("\n        Input before sorting...\n");
 		printarr(arr, n, k);
 	}
 
 	// print arr, k numbers per line
-	private void printarr(int arr[], int n, int k) {
+	private void printarr(double arr[], int n, int k) {
 		int j = 0;
-		for (int i : arr) {
-			prt(i + " ");
-			j++;
-			if (j % k == 0)
-				prt("\n");
+		try{
+			for (double i : arr) {
+				prt(i + " ");
+				j++;
+				if (j % k == 0)
+					prt("\n");
+			}
+		}
+		catch(Exception e){
+			System.out.print("Cannot print numbers");
 		}
 	}
 
 	// sort arr using bubble sort, compute number of
 	// comparison and swaps and print sorted array
-	private void bubblesort(int arr[], int n, int k) {
+	private void bubblesort(double arr[], int n, int k) {
 		int swaps = 0;
 		int comps = n * (n - 1) / 2;
 		for (int i = 1; i < n; i++)
 			for (int j = 0; j < n - i; j++)
 				if (arr[j] > arr[j + 1]) {
-					int tmp = arr[j];
+					double tmp = arr[j];
 					arr[j] = arr[j + 1];
 					arr[j + 1] = tmp;
 					swaps++;
@@ -60,7 +65,8 @@ public class sort2 {
 
 	public static void main(String[] args) throws Exception {
 		sort2 bb = new sort2();
-		int n = 0, a = 0, b = 0, k = 0;
+		int n = 0, k = 0;
+		double a = 0, b = 0;
 		try {
 			Scanner getUserInput = new Scanner(new InputStreamReader(System.in));
 			System.out.println("The number of integers you want generated");
@@ -71,13 +77,13 @@ public class sort2 {
 				}
 			} while (n < 50);
 			System.out.println("Enter a minimum");
-			a = getUserInput.nextInt();
+			a = getUserInput.nextDouble();
 
 			System.out.println("Enter a maximum");
 			do {
-				b = getUserInput.nextInt();
+				b = getUserInput.nextDouble();
 				if (a > b) {
-					System.out.println("Number must be greater than " + Integer.toString(a));
+					System.out.println("Number must be greater than " + Double.toString(a));
 				}
 			} while (a > b);
 
@@ -85,7 +91,7 @@ public class sort2 {
 			do {
 				k = getUserInput.nextInt();
 				if (n < 0) {
-					System.out.println("Must be greater than " + Integer.toString(0));
+					System.out.println("Must be greater than " + Double.toString(0));
 				}
 			} while (n < 0);
 
@@ -94,7 +100,7 @@ public class sort2 {
 		catch (Exception e) {
 			System.out.println("You broke the program.");
 		}
-		int arr[] = new int[n];
+		double arr[] = new double[n];
 		bb.genarr(arr, n, a, b, k);// generate and print
 		bb.bubblesort(arr, n, k); // sort and print
 	}
