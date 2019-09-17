d4615c88279d84e5ab1d20393992cd7230b66a85
Jaime Garcia Garcia <jgarcia16432@gmail.com>
2019-09-17 09:34:28 -0700

    Add professor list program

---
+++ ProfsPrograms/list/invalidinsertion.java
@@ -0,0 +1,6 @@
+/*	invalidinsertion.java	*/
+public class invalidinsertion extends RuntimeException {  
+  public invalidinsertion(int p) {
+	System.out.print("\n" + p + " is invalid Positions for insertion.\n");
+  }
+}// end class invalidinsertion
