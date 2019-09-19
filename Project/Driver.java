5c809754641eeaf9cf03a0f418c711468ff29d03
Jaime Garcia Garcia <jgarcia16432@gmail.com>
2019-09-19 09:19:50 -0700

    Import Prof. main driver code for project 1

---
+++ Project/Driver.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Driver {
+    public static void main(String args[]) {
+        int j, n, m, k, x;
+        try{
+            Scanner inf = new Scanner(System.in);
+            n = inf.nextInt();// read No. of data to read
+            // Create a List of type Integer of size n
+            listarr<Integer> Lint = new listarr2<Integer>(n);
+            // Read n element and insert in a sorted listposition randomly in the list
+            for(j = 1; j <= n; j++){
+                x = inf.nextInt(); // read element
+                Lint.insertsorted (x);
+            }
+            System.out.printf(“The %d inserted data are as follow:”, n);
+            System.out.print(Lint.toString());
+            // read No. of data to search
+            m = inf.nextInt();
+            for(j = 1; j <= m; j++){
+                x = inf.nextInt(); // read data to search
+                k = Lint.binsearch(x);
+                if(k??? //complete it
+                }
+                inf.close();
+            } 
+            catch (Exception e) {
+                prt("Exception " + e + "\n");
+            }
+        }// end main method
+    }
+}
