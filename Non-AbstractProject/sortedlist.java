526aa765cfed408c671238f4ce995d8ed478775b
JaimeGG64 <jgarcia16432@gmail.com>
2019-09-25 22:44:53 -0700

    Add informative comments to sortedlist.java

--- Non-AbstractProject/sortedlist.java
+++ Non-AbstractProject/sortedlist.java
@@ -20,43 +20,61 @@ public class sortedlist{
 	// insert x in a sorted list
 	public void insertsorted(int x){
 		int i;
+		// Adds the first integer to the array
 		if(last == 0){
 			arr[1] = x;
 			last++;
 			System.out.printf("\nInsert %d in a sorted list\n", x);
 			return;
 		}
+		// Place the next integer in the correct order
 		for(i = last; i >= 1 && arr[i] > x; i--){
 			arr[i + 1] =  arr[i];
 		}
+		// Assign the next integer in
 		arr[i + 1] = x;
 		last++;
+		// Print the integer that was just inserted
 		System.out.printf("Insert %d in a sorted list\n", x);
 	}
+
 	// binary search for x in a sorted list
 	public int binsearch(int x){
 		int mid = 0; // Assume x is not found
+		int pointer = 0; // Help point out the which direction the integers is located 
+		int getArrayLength = arr.length - 1; // A manipulable variable to help locate the variable
+		
+		//Prints out the number to to search for
 		System.out.printf("binary search for %d in a sorted list\n", x);
-		int l = 0, r = arr.length - 1; 
-        while (l <= r) { 
-            int m = l + (r - l) / 2; 
-            // Check if x is present at mid 
-            if (arr[m] == x) 
+
+		// This loop will track down the desire integer. If that fails then the method will return 0
+        while (pointer <= getArrayLength) {
+			// Take the length of the array and slip it in half to find the integer.
+			// Base off getArrayLength and pointer the midpoint can change to locate the Integer 
+            mid = pointer + (getArrayLength - pointer) / 2;
+			
+			// Check if the integer in the midpoint 
+            if (arr[mid] == x){
+				// 1 = true
                 return 1; 
-            // If x greater, ignore left half 
-            if (arr[m] < x) 
-                l = m + 1; 
-            // If x is smaller, ignore right half 
-            else
-                r = m - 1; 
-        } 
-        // if we reach here, then element was 
-        // not present 
-        return mid; 
+			}
+			
+			// If the integer is greater, don't check the left 
+            if (arr[mid] < x){
+                pointer = mid + 1; 
+			}
+            // If the integer is less, don't check the right
+			else{
+				getArrayLength = mid - 1; 
+			}
+        }
+        // if we reach here, then element was not present 
+        return mid;
 	}
 
+	// This method will take the array and contruct a string in easy to print format
 	public String toString() {
-		String s = "[";
+		String s = "\n[";
 		for (int i = 1; i < arr.length; i++){
 			if(i == 1){
 				s += arr[i];
@@ -65,14 +83,15 @@ public class sortedlist{
 				s += ", " + arr[i];
 			}
 		}
-		return s + "]\n";
+		return s + "]\n\n";
 	}
 
 	public static void main(String args[]){
 		int j, n, m, k, x;
 		try{
 			Scanner inf = new Scanner(System.in);		
-			n = inf.nextInt();// read No. of data to read
+			// read No. of data to read
+			n = inf.nextInt();
 			
 			// Create a List of type Integer of size n
 			sortedlist Lint = new sortedlist(n);	
@@ -92,10 +111,10 @@ public class sortedlist{
                 x = inf.nextInt();
                 k = Lint.binsearch(x);
                 if(k == 1){
-                    System.out.print(x + " Found\n");
+                    System.out.print(x + " is found!\n");
                 }
                 else{
-                    System.out.print(x + " Not\n");
+                    System.out.print(x + " Ooops is not in the list?\n");
                 }
             }
 			inf.close();
