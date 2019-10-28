12089b3a4d50ca92e954618827cdeb304f566847
JaimeGG64 <jgarcia16432@gmail.com>
2019-10-27 18:12:50 -0700

    Gave Project 3 descriptive comments on how the postfix() method works

--- Project3/driver.java
+++ Project3/driver.java
@@ -1,86 +1,74 @@
 /*
-    CSUN Department of Computer Science
-    COMP 182 Data Structures and Program Design Fall 2019
-    Project 3 (100 Pts), Infix to Postfix - Due: Oct. 31, 19
-
-    NOTE: Projects will not be graded if:
-    - Your complete project is not in a SINGLE xxxxx.java file, where xxxxx is the first 5 characters of your last name.
-    - It does not read input from System.in. i.e. command prompt.
-
-    Write an efficient java program to convert and print an infix expression to postfix expression. You can use Java stack methods. Assume your java program is in xxxxx.java file, where xxxxx is the first 5 characters of your last name.
+    Name: Jaime Garcia Garcia
+    Date: 10-27-2019
+    Application: Infix to Postfix
+    Description: This program accepts a text file with infix math expression and convert them into postfix expression.
     
-    To compile: 
-    ```bash
-    $ javac xxxxx.java
-    ```
-    
-    To execute:
-    ```bash 
-    $ java xxxxx < any data file name
-    ```
+    How to Run the Program:
+    1. Compile Program
+        ```bash
+        $ javac driver.java
+        ```
 
-    Your main method should be as follow:
-    ```java
-        public static void main(String args[]) {
-            xxxxx p = new xxxxx (); // xxxxx is the first 5 characters of your last name
-            String iexp, pexp;  //infix postfix expression            
-            try{
-                Scanner inf = new Scanner (System.in);                    
-                // Read input from KB/ File
-                while(inf.hasNext()){
-                    // read next infix expression                                
-                    iexp = inf.next();
-                    // Assume method name to convert infix to postfix is postfix
-                    pexp = p.postfix(iexp);
-                    System.out.printf("\nPosfix of %s is: %s", iexp, pexp);
-                }// end while
-                inf.close();// close input file
-            }
-            catch (Exception e) {
-                prt("\nException " + e + "\n");
-            }
-        }// end main method
-    ```
+    2. Execute Program with text file
+        ```bash
+        $ java driver < <YourTextFile>.txt
+        ```
 */
 
 import java.util.Stack;
 import java.util.Scanner;
 
 public class driver {
-
     public String postfix(String infixToPostfixString){
+        // Preparing variables and data structure the postfix()  
         char tempchar;
-        Stack<Character> operandStack = new Stack<Character>();
+        Stack<Character> operatorStack = new Stack<Character>();
         Stack<Character> outputStack = new Stack<Character>();
         int lengthOfString =  infixToPostfixString.length();
+        
+        // The For Loop goes through the Infix string one character at the time.
         for(int i = 0; i < lengthOfString; i++){
+            // Temporarly sort the inputed string character base of index i
             tempchar = infixToPostfixString.charAt(i);
+            
+            // Check if character is a Letter, then the character is push to the output stack.
             if(Character.isLetter(tempchar)){
                 outputStack.push(tempchar);
             }
+            // If character is '(' it will be push to the operatorStack 
             else if(tempchar == '('){
-                operandStack.push(tempchar);
+                operatorStack.push(tempchar);
             }
+            // If character is ')' it will push the operator to the output until it reach the ')' which will be pop off after the loop 
             else if(tempchar == ')'){
-                while(!operandStack.isEmpty() && operandStack.peek() != '('){
-                    outputStack.push(operandStack.pop());
+                while(!operatorStack.isEmpty() && operatorStack.peek() != '('){
+                    outputStack.push(operatorStack.pop());
                 }
-                operandStack.pop();
+                operatorStack.pop();
             }
+            // Base off the presedence of the operators and operator stack, the operator stack will pop and the push to the output stack. 
+            // Then the operator character will be pushed to operator stack 
             else{
-                while (!operandStack.isEmpty() && checkPresedence(tempchar) <= checkPresedence(operandStack.peek())){ 
-                    outputStack.push(operandStack.pop()); 
+                while (!operatorStack.isEmpty() && checkPresedence(operatorStack.peek()) >= checkPresedence(tempchar)){ 
+                    outputStack.push(operatorStack.pop()); 
                 } 
-                operandStack.push(tempchar); 
+                operatorStack.push(tempchar); 
             }
         }// End For Loop
-        while(!operandStack.isEmpty()){
-            outputStack.push(operandStack.pop()); 
+
+        // Clear out any remaining operators in the operator stack to the output stack
+        while(!operatorStack.isEmpty()){
+            outputStack.push(operatorStack.pop()); 
         }
-        infixToPostfixString = outputStack.toString().replaceAll("\\[", "").replaceAll("]", "").replaceAll(",", "");
+
+        // Convert the output stack to a string and strip out all special text formating.
+        infixToPostfixString = outputStack.toString().replaceAll("\\[", "").replaceAll("]", "").replaceAll(",", "").replaceAll(" ", "");
+        
         return infixToPostfixString;
     }
 
+    // checkPresedence() will check which operator has the hightest precedence
     public int checkPresedence(char charToCheck){
         if(charToCheck == '^'){
             return 3;
