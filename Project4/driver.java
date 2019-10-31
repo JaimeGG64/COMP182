77a9d5094d66504d47e07b4e0466d688eec52b1b
Jaime Garcia Garcia <jgarcia16432@gmail.com>
2019-10-31 12:42:39 -0700

    I think complete the program with the require dequeue methods

--- Project4/driver.java
+++ Project4/driver.java
@@ -90,60 +90,126 @@
 */
 
 import java.util.Scanner;
-import java.util.LinkedList;
 
 public class driver {
+    Node front, rear;
+    class Node {
+		Node ll, rl;
+		int data;
+
+		Node(int x) {
+			data = x;
+			ll = rl = null;
+		}
+	}
 
     public void addfront(int x) {
-        System.out.print("Add Front");
+        Node tmp = new Node(x);
+		if(front == null){
+            front = tmp;
+            rear = tmp;
+        }
+        else{
+            tmp.rl = front;
+            front.ll = tmp;
+            front = tmp;
+        }
     }
 
     public void printfwd() {
-        System.out.print("Print Forward");
+        for(Node cur = front; cur != null; cur = cur.rl){
+            System.out.print(cur.data + " ");
+        }
+        System.out.print("\n");
     }
 
     public int delfront() {
-        return 0;
+        if(front == null){
+            return 0;
+        }
+        else{
+            if(front == rear){
+                front = null;
+                rear = null;
+                return 1;
+            }
+            else{
+                Node tmp = front.rl;
+                front = tmp;
+                front.ll = null;
+                return 1;
+            }
+        }
     }
 
-    public int addrear(int x) {
-        return 0;
+    public void addrear(int x) {
+        Node tmp = new Node(x);
+		if(rear == null){
+            rear = tmp;
+            front = tmp;
+        }
+        else{
+            tmp.ll = rear;
+            rear.rl = tmp;
+            rear = tmp;
+        }
     }
 
     public int delrear() {
-        return 0;
+        if(rear == null){
+            return 0;
+        }
+        else{
+            if(rear == front){
+                rear = front = null;
+                return 1;
+            }
+            else{
+                Node tmp = rear.ll;
+                rear = tmp;
+                rear.rl = null;
+                return 1;
+            }
+        }
     }
 
     public void printbwd() {
-        System.out.print("hi");
+        for(Node cur = rear; cur != null; cur = cur.ll){
+            System.out.print(cur.data + " ");
+        }
+        System.out.print("\n");
     }
 
     public static void main(String args[]) {
-        driver dq = new driver(); // driver is the first 5 characters of your last name
+        // driver is the first 5 characters of your last name
+        driver dq = new driver(); 
         try {
             Scanner inf = new Scanner(System.in);
-
+            int x, j;
             // Read input from KB/ File
             while (inf.hasNext()) {
                 // read operation type (optype):
                 /*
-                 * 1:add front 2:delete front 3:add rear and delete rear
+                    1:add front 
+                    2:delete front 
+                    3:add rear and delete rear
                  */
                 int optype = inf.nextInt();
 
                 switch (optype) {
-                case 1:
-                    int x = inf.nextInt(); // read x
+                case 1: 
+                    // read x
+                    x = inf.nextInt(); 
                     dq.addfront(x);
                     dq.printfwd(); // print forward dequeuer elements
                     break;
                 case 2:
-                    int j = dq.delfront();// delete front
+                    j = dq.delfront();// delete front
                     if (j == 1) {
                         dq.printbwd(); // print backward dequeuer elements
                     }
                     break;
-                case 3:
+                case 3: 
                     x = inf.nextInt(); // read x
                     dq.addrear(x);
                     dq.printfwd(); // print forward dequeuer elements
@@ -155,9 +221,7 @@ public class driver {
                     }
                 }// end switch
             } // end while
-
             inf.close();// close input file
-
         } catch (Exception e) {
             System.out.print("\nException " + e + "\n");
         }
