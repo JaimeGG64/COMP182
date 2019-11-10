bd5f54b189833ebf10f0c938b49039d3c1a02ebb
Jaime Garcia Garcia <jgarcia16432@gmail.com>
2019-11-09 18:21:44 -0800

    Add Program description to Project 2

--- Project4/driver.java
+++ Project4/driver.java
@@ -1,92 +1,19 @@
 /*
-    Write an efficient java program to implement an integer doubly linked list
-    Dequeue which insertion and deletion can be than at either end. You have to
-    write 6 methods: add front, delete front, add rear, delete rear, print
-    forward (left to right) and print backward (right to left). After each
-    addition or deletion dequeue elements are printed forward or backward
-    respectively.
+    Program: Doubly Linked List Dequeue 
+    Name: Jaime Garcia Garcia
+    Date: 10-31-2019
+    Description: 
 
-    Assume your java program is in driver.java file, where driver is the first 5
-    characters of your last name.
+    How to Run The Program:
+    1. How to Compile:
+        ```bash
+        $ javac garciaj.java
+        ```
 
-    To compile: javac driver.java
-
-    To execute: java driver < any data file name
-
-    Your main method should be as follow:
-
-    public static void main(String args[]) {
-
-    driver dq = new driver (); // driver is the first 5 characters of your last name
-
-    try{
-
-    Scanner inf = new Scanner (System.in);
-
-    // Read input from KB/ File
-
-    while(inf.hasNext()){
-
-    // read operation type (optype):
-
-    //1:add front, 2:delete front, 3:add rear and delete rear
-
-    optype = inf.nextInt();
-
-    switch (optype){
-
-    case 1: x = inf.nextInt(); // read x
-
-    dq.addfront(x);
-
-    dq.printfwd(); // print forward dequeuer elements
-
-    break;
-
-    case 2: j = dq.delfront();// delete front
-
-    if(j……….
-
-    dq.printbwd(); // print backward dequeuer elements
-
-    break;
-
-    case 3: x = inf.nextInt(); // read x
-
-    dq.addrear(x);
-
-    dq.printfwd(); // print forward dequeuer elements
-
-    break;
-
-    default: j = dq.delrear();// delete front
-
-    if(j……….
-
-    dq.printbwd(); // print backward dequeuer elements
-
-    }//end switch
-
-    }// end while
-
-    inf.close();// close input file
-
-    }catch (Exception e) {prt("\nException " + e + "\n");}
-
-    }// end main method
-
-    NOTE: Projects will not be graded if:
-
-    Your complete project is not in a SINGLE driver.java file, where driver is the
-    first 5 characters of your last name.
-
-    It does not read input from System.in. i.e. command prompt.
-
-
-
-    Sample data file can be as follow:
-
-    1 100 2 2 3 500 3 600 3 250 4 1 150 1 220 2 4 1 50 3 55 1 20 1 80 3 90
+    2. How to Execute
+        ```bash
+        $ java garcia < <YourTextFile>.txt
+        ```
 */
 
 import java.util.Scanner;
