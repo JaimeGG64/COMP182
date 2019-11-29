7a651d7e49aa7033e8b4077907e8ce3040be5433
JaimeGG64 <jgarcia16432@gmail.com>
2019-11-28 17:08:17 -0800

    Add selection sort to the sorting collection

--- SortingCollection/sortingCollection.java
+++ SortingCollection/sortingCollection.java
@@ -27,10 +27,31 @@ public class sortingCollection<T extends Comparable<? super T>>{
         }
     }
 
+    public void selectionSort(T[] array){
+        int n = array.length; 
+        // One by one move boundary of unsorted subarray 
+        for (int i = 0; i < n-1; i++) { 
+            // Find the minimum element in unsorted array 
+            int min_idx = i; 
+            for (int j = i+1; j < n; j++){
+                if (array[j].compareTo(array[min_idx]) < 0){
+                    min_idx = j; 
+                }
+            } 
+            T temp = array[min_idx]; 
+            array[min_idx] = array[i]; 
+            array[i] = temp; 
+        } 
+    }
+
     public static void main(String[] args) {
         Double[] arrayOfDoubles = {0.35, 0.02, 0.36, 0.82, 0.27, 0.49, 0.41, 0.17, 0.30, 0.89, 0.37, 0.66, 0.82, 0.17, 0.20, 0.96, 0.18, 0.25, 0.37, 0.52};
-        sortingCollection<Double> doubleSorter   = new sortingCollection<Double>();
-        doubleSorter.insertionSort(arrayOfDoubles);
+        sortingCollection<Double> doubleSorter = new sortingCollection<Double>();
+        doubleSorter.selectionSort(arrayOfDoubles);
+        String[] arrayOfStrings = {"Jack" , "Emily" , "Grace" , "Emma", "James" , "Noah"};
+        sortingCollection<String> stringSorter = new sortingCollection<String>();
+        stringSorter.insertionSort(arrayOfStrings);
         System.out.println(java.util.Arrays.toString(arrayOfDoubles));
+        System.out.print(java.util.Arrays.toString(arrayOfStrings));
     }
 }
